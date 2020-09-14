01aeed13ef7c51ec1b84b7305c54fd913d531df1
Alfaroque Islam <alfaroque5@gmail.com>
2020-09-14 18:32:06 -0400

    CS Unit 1 Week 3 Assignment 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,19 +7,28 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+
+    return arr
 
         # TO-DO: swap
         # Your code here
-
-    return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n-1):  
+        for j in range(0, n-i-1):              
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
     return arr
 
 '''
